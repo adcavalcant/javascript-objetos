ecbfacfd8bff5422608b2e702083ee21a5d277e3
adcavalcant <aderc19@gmail.com>
2023-07-06 13:26:42 -0300

    adicionando/alterando atributos do objeto

---
+++ alterar.js
@@ -0,0 +1,21 @@
+const pessoa = {
+  nome: "Luma",
+  profissao: "Engenheira",
+};
+
+console.log(`${pessoa.nome} é ${pessoa.profissao}`);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "(61) 981305686";
+console.log(pessoa.telefone);
+
+pessoa.nome = "Luma Silva";
+
+console.log(pessoa);
+
+const novaPessoa = {
+  nome: "Pedro",
+};
+
+pessoa = novaPessoa;
+
