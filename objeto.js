182e027c4141ace4a9881520357cae80bf7e3780
adcavalcant <aderc19@gmail.com>
2023-07-05 22:21:14 -0300

    Exemplo de objeto

---
+++ objeto.js
@@ -0,0 +1,9 @@
+const listaCpfs = [{ Ana: 111111 }, { Claudio: 222222 }, { Fernanda: 333333 }];
+
+const objetoPessoa = {
+  nome: "José",
+  idade: 32,
+  cpf: 813292871932,
+};
+
+console.log(objetoPessoa);
