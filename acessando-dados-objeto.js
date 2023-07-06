9cab04b441d6785d2c65434ab3877526811e1bff
adcavalcant <aderc19@gmail.com>
2023-07-05 22:25:35 -0300

    acessando dados do objeto

---
+++ acessando-dados-objeto.js
@@ -0,0 +1,12 @@
+const cliente = {
+  nome: "André",
+  idade: 32,
+  cpf: "21323144324",
+  email: "andre@dominio.com",
+};
+
+console.log(cliente);
+console.log(cliente.nome);
+console.log(cliente.idade);
+console.log(cliente.cpf);
+console.log(cliente.email);
