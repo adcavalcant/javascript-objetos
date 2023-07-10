5d2b7e443324b8e3c060d4a0b3a39b12d22b8479
adcavalcant <aderc19@gmail.com>
2023-07-10 12:29:58 -0300

    Operações com JSON

--- leitura.js
+++ leitura.js
@@ -2,3 +2,12 @@ const dados = require("./cliente.json");
 
 console.log(dados);
 console.log(typeof dados);
+
+const clienteEmString = JSON.stringify(dados);
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString);
+console.log(clienteEmString.length);
+
+const objetoCliente = JSON.parse(clienteEmString);
+console.log(typeof objetoCliente);
